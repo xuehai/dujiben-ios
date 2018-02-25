45dcda1c8cf0219744ef154dc9845b94d8894976
xuehai <xuehaizeng@gmail.com>
2018-02-25 11:28:47 +0800

    add table view

--- dujiben-ios/RootViewController.swift
+++ dujiben-ios/RootViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class RootViewController: UIViewController {
+class RootViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView : UITableView!
+    let cellIdentifier = "TableViewCell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+        buildUI()
+    }
+    
+    func buildUI(){
         self.title = "读记本"
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        
+        let addButton : UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add, target: self, action: #selector(addButtonPressed))
+        self.navigationItem.rightBarButtonItem = addButton
+    }
+    
+    @objc func addButtonPressed(sender: UIBarButtonItem){
+        print("to add a book")
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,5 +45,19 @@ class RootViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func numberOfSections(in tableView: UITableView) -> Int{
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as UITableViewCell
+        cell.textLabel?.text = "cell"
+        return cell;
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return 1
+    }
 
 }
